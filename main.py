d73814c508688bbfa3fbfd6d4f6bf2d6d54e753e
IchiKnee <ronn.delosreyes@ucalgary.ca>
2022-05-01 16:24:32 -0600

    Added giving L's to Korbine and added README

---
+++ main.py
@@ -0,0 +1,40 @@
+# main.py
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import time
+import asyncio
+
+load_dotenv()
+TOKEN = os.getenv('CPL_DEV_TOKEN')
+
+client = discord.Client()
+
+# Welcome message
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+# Event where a message is sent on discord
+@client.event
+async def on_message(msg):
+
+    # Parse username and channel name
+    username = str(msg.author).split('#')[0]
+    channel = str(msg.channel.name)
+
+    # Don't react to itself
+    if msg.author == client.user:
+        return
+
+    # Give Korbine an L
+    if "rokuro" in username.lower():
+        await msg.add_reaction('\U0001F1F1')
+
+    # Continue the chain gif train
+    if ".gif" in msg.content:
+        await msg.channel.send(msg.content)
+        await asyncio.sleep(5)
+
+client.run(TOKEN)
